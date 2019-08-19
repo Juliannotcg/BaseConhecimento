64722453c61f1c3700e5db9f27ce8470d9c2c581
T088166146 <T088166146@MIRANTENTB006>
2019-08-19 14:55:29 -0300

    Subindo ajustes no thema.

--- src/fuse-configs/fuseThemesConfig.js
+++ src/fuse-configs/fuseThemesConfig.js
@@ -6,13 +6,17 @@ export const fuseThemesConfig = {
     default    : {
         palette: {
             type     : 'light',
-            primary  : fuseDark,
-            secondary: {
-                light: lightBlue[400],
-                main : lightBlue[600],
-                dark : lightBlue[700]
+            primary  : {
+                light       : '#87efff',
+                main        : '#4dbce9',
+                dark        : '#008cb7',
+                contrastText: '#fff'
             },
-            error    : red
+            secondary: {
+                light: '#ffff83',
+                main : '#d1e751',
+                dark : '#9db516'
+            }
         },
         status : {
             danger: 'orange'
