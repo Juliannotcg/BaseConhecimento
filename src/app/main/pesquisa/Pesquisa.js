ca980944a34f8f399dd9aec98f1ac4fc239bd8e7
T088166146 <T088166146@MIRANTENTB006>
2019-08-30 13:24:48 -0300

    Iniciando implementação do pesquisar

---
+++ src/app/main/pesquisa/Pesquisa.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FusePageCarded } from '@fuse';
+
+
+function Pesquisa() {
+
+    return (
+        <React.Fragment>
+            <FusePageCarded/>
+        </React.Fragment>
+    );
+}
+
+export default Pesquisa;
+
+
